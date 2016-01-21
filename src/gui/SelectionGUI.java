89bea4930c5c0e954beafbfee926fb16a0545191
leethaxor <felix.beutter@outlook.com>
2016-01-21 19:17:40 +0100

    Added 'Strings' class, etc.

--- src/gui/SelectionGUI.java
+++ src/gui/SelectionGUI.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
@@ -22,7 +25,6 @@ import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
-import essentials.Essentials;
 import main.Main;
 
 public class SelectionGUI {
@@ -30,11 +32,11 @@ public class SelectionGUI {
 	static JFrame frame;
 	static JPanel panel, pnlScenario;
 	static GridBagLayout layout;
-	static JLabel lblPath, lblMods, lblName, lblScenarios;
-	static JTextField txfPath, txfName;
+	static JLabel lblPath, lblMods, lblName, lblScenarios, lblScenarioName;
+	static JTextField txfPath, txfName, txfScenarioName;
 	static JButton btnPath, btnContinue, btnAccept;
 	static JFileChooser chooser;
-	static JComboBox<String> cbxMods;
+	static JComboBox<String> cbxMods, cbxScenarios;
 	static File file = new File("");
 	static String path = "D:\\Programme\\Steam\\SteamApps\\common\\For The Glory\\";
 	static boolean finishedLoading = false;
@@ -54,12 +56,12 @@ public class SelectionGUI {
 		panel = new JPanel();
 		panel.setLayout(layout);
 		panel.setBackground(Main.clrBackground);
-		Essentials.addComponent(frame, layout, panel, 0, 0, 1, 1, 1, 1, new Insets(0, 0, 0, 0));
+		addComponent(frame, layout, panel, 0, 0, 1, 1, 1, 1, new Insets(0, 0, 0, 0));
 
 		lblPath = new JLabel("Pfad:", SwingConstants.LEFT);
 		lblPath.setForeground(Main.clrFont);
 		lblPath.setFont(Main.fntBold);
-		Essentials.addComponent(panel, layout, lblPath, 0, 0, 1, 1, 1, 0, new Insets(10, 10, 10, 10));
+		addComponent(panel, layout, lblPath, 0, 0, 1, 1, 1, 0, new Insets(10, 10, 10, 10));
 
 		chooser = new JFileChooser();
 		chooser.setMultiSelectionEnabled(false);
@@ -68,29 +70,29 @@ public class SelectionGUI {
 		chooser.setDialogTitle("'For The Glory'-Ordner auswählen");
 
 		txfPath = new JTextField(path);
-		txfPath.setEditable(false);
+		txfPath.setEditable(true);
 		txfPath.setForeground(Main.clrFont);
 		txfPath.setBackground(Color.white);
 		txfPath.setPreferredSize(new Dimension(300, 20));
-		Essentials.addComponent(panel, layout, txfPath, 0, 1, 2, 1, 1, 0, new Insets(0, 10, 10, 10));
+		addComponent(panel, layout, txfPath, 0, 1, 2, 1, 1, 0, new Insets(0, 10, 10, 10));
 
 		lblMods = new JLabel("Mod auswählen", SwingConstants.LEFT);
 		lblMods.setForeground(Color.black);
 		lblMods.setFont(Main.fntBold);
-		Essentials.addComponent(panel, layout, lblMods, 0, 3, 2, 1, 1, 0, new Insets(10, 10, 5, 10));
+		addComponent(panel, layout, lblMods, 0, 3, 2, 1, 1, 0, new Insets(10, 10, 5, 10));
 		lblMods.setVisible(false);
 
-		lblName = new JLabel("Name", SwingConstants.LEFT);
+		lblName = new JLabel("Mod Name", SwingConstants.LEFT);
 		lblName.setForeground(Color.black);
 		lblName.setFont(new Font("Verdana", 0, 12));
-		Essentials.addComponent(panel, layout, lblName, 0, 5, 2, 1, 1, 0, new Insets(5, 10, 5, 10));
+		addComponent(panel, layout, lblName, 0, 5, 2, 1, 1, 0, new Insets(5, 10, 5, 10));
 		lblName.setVisible(false);
 
 		txfName = new JTextField();
 		txfName.setEditable(true);
 		txfName.setForeground(Color.black);
 		txfName.setBackground(Color.white);
-		Essentials.addComponent(panel, layout, txfName, 0, 6, 2, 1, 1, 0, new Insets(0, 10, 10, 10));
+		addComponent(panel, layout, txfName, 0, 6, 2, 1, 1, 0, new Insets(0, 10, 10, 10));
 		txfName.setVisible(false);
 
 		btnPath = new JButton("auswählen");
@@ -108,62 +110,61 @@ public class SelectionGUI {
 				}
 			}
 		});
-		Essentials.addComponent(panel, layout, btnPath, 1, 0, 1, 1, 0, 0, new Insets(10, 0, 10, 10));
+		addComponent(panel, layout, btnPath, 1, 0, 1, 1, 0, 0, new Insets(10, 0, 10, 10));
 
-		Essentials.addComponent(panel, layout, new JPanel(), 0, 2, 1, 1, 1, 0, new Insets(0, 0, 0, 0));
+		addComponent(panel, layout, new JPanel(), 0, 2, 1, 1, 1, 0, new Insets(0, 0, 0, 0));
 
 		btnAccept = new JButton("weiter");
 		btnAccept.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				
 
-//				Thread loadingThread = new Thread(new Runnable() {
-//
-//					@Override
-//					public void run() {
-//
-//						Thread loadLoadingGUI = new Thread(new Runnable() {
-//
-//							@Override
-//							public void run() {
-//
-//								try {
-//									loadGUI();
-//								} catch (IOException | InterruptedException e) {
-//
-//									e.printStackTrace();
-//								}
-//							}
-//						});
-//						loadLoadingGUI.start();
-//
-//						try {
-//							new GUI();
-//						} catch (Exception e) {
-//							e.printStackTrace();
-//						}
-//					}
-//				});
-//
-//				if (cbxMods.getSelectedItem().equals("Neuen Mod erstellen")) {
-//
-//					if (txfName.getText().trim() != null && !txfName.getText().trim().equals("")) {
-//
-//						loadingThread.start();
-//					} else
-//						JOptionPane.showMessageDialog(null, "Bitte geben Sie einen Namen ein!", "Fehler  aufgetreten",
-//								JOptionPane.ERROR_MESSAGE);
-//				} else
-//					loadingThread.start();
+				// Thread loadingThread = new Thread(new Runnable() {
+				//
+				// @Override
+				// public void run() {
+				//
+				// Thread loadLoadingGUI = new Thread(new Runnable() {
+				//
+				// @Override
+				// public void run() {
+				//
+				// try {
+				// loadGUI();
+				// } catch (IOException | InterruptedException e) {
+				//
+				// e.printStackTrace();
+				// }
+				// }
+				// });
+				// loadLoadingGUI.start();
+				//
+				// try {
+				// new GUI();
+				// } catch (Exception e) {
+				// e.printStackTrace();
+				// }
+				// }
+				// });
+				//
+				// if (cbxMods.getSelectedItem().equals("Neuen Mod erstellen"))
+				// {
+				//
+				// if (txfName.getText().trim() != null &&
+				// !txfName.getText().trim().equals("")) {
+				//
+				// loadingThread.start();
+				// } else
+				// JOptionPane.showMessageDialog(null, "Bitte geben Sie einen
+				// Namen ein!", "Fehler aufgetreten",
+				// JOptionPane.ERROR_MESSAGE);
+				// } else
+				// loadingThread.start();
 			}
 		});
-		Essentials.addComponent(panel, layout, btnAccept, 1, 7, 1, 1, 0, 0, new Insets(0, 10, 10, 10));
+		addComponent(panel, layout, btnAccept, 1, 7, 1, 1, 0, 0, new Insets(0, 10, 10, 10));
 		btnAccept.setVisible(false);
-		
-		
 
 		btnContinue = new JButton("weiter");
 		btnContinue.addActionListener(new ActionListener() {
@@ -209,7 +210,7 @@ public class SelectionGUI {
 								}
 							}
 						});
-						Essentials.addComponent(panel, layout, cbxMods, 0, 4, 2, 1, 1, 0, new Insets(0, 10, 10, 10));
+						addComponent(panel, layout, cbxMods, 0, 4, 2, 1, 1, 0, new Insets(0, 10, 10, 10));
 						cbxMods.setVisible(false);
 
 						lblMods.setVisible(true);
@@ -232,15 +233,33 @@ public class SelectionGUI {
 				}
 			}
 		});
-		Essentials.addComponent(panel, layout, btnContinue, 1, 2, 1, 1, 0, 0, new Insets(0, 10, 10, 10));
+		addComponent(panel, layout, btnContinue, 1, 2, 1, 1, 0, 0, new Insets(0, 10, 10, 10));
 
 		pnlScenario = new JPanel();
 		pnlScenario.setLayout(layout);
 		pnlScenario.setBackground(Main.clrBackground);
-		Essentials.addComponent(panel, layout, pnlScenario, 0, 10, 1, 1, 1, 1, new Insets(10, 10, 10, 10));
-		
-//		lblScenarios = new JLabel("Szenario auswählen")
-		
+		pnlScenario.setVisible(false);
+		addComponent(panel, layout, pnlScenario, 0, 10, 1, 1, 1, 1, new Insets(10, 10, 10, 10));
+
+		lblScenarios = new JLabel("Szenario auswählen", SwingConstants.LEFT);
+		lblScenarios.setFont(Main.fntBold);
+		lblScenarios.setForeground(Main.clrFont);
+		addComponent(pnlScenario, layout, lblScenarios, 0, 0, 1, 1, 1, 0, new Insets(0, 10, 5, 10));
+
+		cbxScenarios = new JComboBox<>();
+		addComponent(pnlScenario, layout, cbxScenarios, 0, 1, 1, 1, 1, 0, new Insets(0, 10, 10, 10));
+
+		lblScenarioName = new JLabel("Szenario Name");
+		lblScenarioName.setFont(Main.fntStandard);
+		lblScenarioName.setForeground(Main.clrFont);
+		addComponent(pnlScenario, layout, lblScenarioName, 0, 2, 1, 1, 1, 0, new Insets(0, 10, 5, 10));
+
+		txfScenarioName = new JTextField();
+		txfScenarioName.setEditable(true);
+		txfScenarioName.setForeground(Color.black);
+		txfScenarioName.setBackground(Color.white);
+		addComponent(pnlScenario, layout, txfScenarioName, 0, 3, 1, 1, 1, 0, new Insets(0, 10, 10, 10));
+
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
@@ -269,18 +288,18 @@ public class SelectionGUI {
 		JLabel label = new JLabel("Laden...");
 		label.setForeground(Main.clrFont);
 		label.setFont(Main.fntBold);
-		Essentials.addComponent(panel, layout, label, 0, 0, 1, 1, 0, 0, new Insets(5, 5, 5, 5));
+		addComponent(panel, layout, label, 0, 0, 1, 1, 0, 0, new Insets(5, 5, 5, 5));
 
-		Essentials.addComponent(panel, layout, new JPanel(), 1, 0, 1, 1, 1, 0, new Insets(0, 0, 0, 0));
+		addComponent(panel, layout, new JPanel(), 1, 0, 1, 1, 1, 0, new Insets(0, 0, 0, 0));
 
 		JLabel gif = new JLabel(icon);
-		Essentials.addComponent(panel, layout, gif, 2, 0, 1, 1, 0, 0, new Insets(5, 0, 5, 5));
+		addComponent(panel, layout, gif, 2, 0, 1, 1, 0, 0, new Insets(5, 0, 5, 5));
 
 		gif.setVisible(false);
 
 		JProgressBar progressBar = new JProgressBar();
 		progressBar.setIndeterminate(true);
-		Essentials.addComponent(panel, layout, progressBar, 0, 1, 3, 1, 1, 0, new Insets(5, 5, 5, 5));
+		addComponent(panel, layout, progressBar, 0, 1, 3, 1, 1, 0, new Insets(5, 5, 5, 5));
 
 		frame.pack();
 		frame.setLocationRelativeTo(null);
@@ -304,5 +323,21 @@ public class SelectionGUI {
 		if (cbxMods.getSelectedItem().equals("Neuen Mod erstellen"))
 			return txfName.getText().trim();
 		return (String) cbxMods.getSelectedItem();
+	}
+
+	static void addComponent(Container cont, GridBagLayout gbl, Component c, int x, int y, int width, int height,
+			double weightx, double weighty, Insets insets) {
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = x;
+		gbc.gridy = y;
+		gbc.gridwidth = width;
+		gbc.gridheight = height;
+		gbc.weightx = weightx;
+		gbc.weighty = weighty;
+		gbc.insets = insets;
+		gbl.setConstraints(c, gbc);
+		cont.add(c);
 	}
 }
---
+++ src/gui/Strings.java
@@ -0,0 +1,20 @@
+package gui;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class Strings {
+	
+	private static final String BUNDLE_NAME = "scenarioeditor.strings";
+	
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+			.getBundle(BUNDLE_NAME);
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return "! error !";
+		}
+	}
+}
